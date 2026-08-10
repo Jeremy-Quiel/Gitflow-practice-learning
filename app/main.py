aafc73b66391c100c0325c7ee708747d909f6548
Jarvis <ing.jeremy.quiel@gmail.com>
2026-08-09 18:48:30 -0600

    feat(main.py) agregar opciones principales al main

---
+++ app/main.py
@@ -0,0 +1,10 @@
+while True:
+    print(f"="*20)
+
+    print(f"Tienda de JarTech")
+    print(f"(1) Comprar")
+    print(f"(2) Vender")
+    print(f"(0) Salir")
+    print(f"="*20)
+
+    input("Opcion: ")
