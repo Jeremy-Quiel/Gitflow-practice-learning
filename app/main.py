e2dac4309e5cb7ed8c2873ef31696a61ae7a6afa
Jarvis <ing.jeremy.quiel@gmail.com>
2026-08-09 19:06:43 -0600

    refactor(main.py) cambiar descripcion de opciones del main.py

--- app/main.py
+++ app/main.py
@@ -2,9 +2,9 @@ while True:
     print(f"="*20)
 
     print(f"Tienda de JarTech")
-    print(f"(1) Comprar")
-    print(f"(2) Vender")
-    print(f"(0) Salir")
+    print(f"(1) Comprar Productos")
+    print(f"(2) Vender Productos")
+    print(f"(0) Salir de la app")
     print(f"="*20)
 
     input("Opcion: ")
