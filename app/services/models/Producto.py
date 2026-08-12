7ae93335be904d7908966d2b3fc885260f84c2b6
Jarvis <ing.jeremy.quiel@gmail.com>
2026-08-11 23:02:48 -0600

    fix: fix bug commit

--- app/services/models/Producto.py
+++ app/services/models/Producto.py
@@ -1,18 +1,10 @@
 class Producto:
-<<<<<<< HEAD
     def __init__(self, id, nombre, precio, tipo, marca):
         self.__id = id
         self.nombre = nombre
         self.precio = precio
         self.tipo = tipo
         self.marca = marca
-=======
-    def __init__(self, nombre, precio, tipo, marca):
-        self.__nombre = nombre
-        self.__precio = precio
-        self.__tipo = tipo
-        self.__marca = marca
->>>>>>> develop
 
     @property
     def id(self):
@@ -43,7 +35,6 @@ class Producto:
     def tipo(self):
         return self.__tipo
 
-
     @tipo.setter
     def tipo(self, tipo):
         self.__tipo = tipo
@@ -52,24 +43,15 @@ class Producto:
     def marca(self):
         return self.__marca
     
-
     @marca.setter
     def marca(self, marca):
         self.__nombrmarca = marca
 
-
     def __str__(self):
-<<<<<<< HEAD
         to_str = f"Id: {self.__id}"
         to_str += f"Nombre: {self.nombre}\n"
         to_str += f"Precio: {self.precio}\n"
         to_str += f"Tipo: {self.tipo}\n"
         to_str += f"Marca: {self.marca}\n"
-=======
-        to_str = f"Nombre: {self.__nombre}\n"
-        to_str += f"Precio: {self.__precio}\n"
-        to_str += f"Tipo: {self.__tipo}\n"
-        to_str += f"Marca: {self.__marca}\n"
->>>>>>> develop
 
         return to_str
